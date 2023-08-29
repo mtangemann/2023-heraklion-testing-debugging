f7daf779e9d52c92a2ee24fcf9bdf2df25ee89f4
ASPP Student <student@localhost>
2023-08-29 09:47:39 +0300

    add find_maxima method

--- hands_on/local_maxima/local_maxima.py
+++ hands_on/local_maxima/local_maxima.py
@@ -7,4 +7,10 @@ def find_maxima(x):
     Output:
     idx -- list of indices of the local maxima in x
     """
-    return []
+    maxima = []
+
+    for index in range(1, len(x)-1):
+        if x[index] > x[index-1] and x[index] > x[index+1]:
+            maxima.append(index)
+
+    return maxima
